f5507542e52338e114ea9ebfa309232cf7646ef1
Dome-san <dimasmdv.seo@gmail.com>
2023-06-20 11:11:56 +0200

    subimos carpeta imagenes,css,crear nav y vimos el proceso clone de git hub, ocultamos las keys

--- notas.md
+++ notas.md
@@ -1,3 +1,13 @@
 En este archivo, anotaremos todos los problemas o cosas que queramos destacar del proyecto
 
 Podemos anadir si nos sobra tiempo tambien para reservar un hotel, se puede hacer con la API AMADEUS hospitality
+
+Dos idiomas
+
+Scroll imagenes detras del buscador
+
+ocultar apikey apisecret
+
+posicionamiento de los aviones
+
+meter imagenes toggler en carpeta y quitar del html
--- test.js
+++ test.js
@@ -1,20 +1,32 @@
 //We are testing how to use the API
 
-const apiKey = "Ae0VxGIgwo77Mo2DYPAs24Of1jt3Uhua";
-const apiSecret = "CEMm5mDrVXY9aGcL";
-
 // Set the endpoint URL
 const url = "https://test.api.amadeus.com/v1/security/oauth2/token";
 
-// Set the request parameters
-const data = new URLSearchParams();
-data.append("grant_type", "client_credentials");
-data.append("client_id", apiKey);
-data.append("client_secret", apiSecret);
+const getKeys = async () => {
+  try {
+    const res = await fetch("./keys.json");
+    const data = await res.json();
+    return [data.apiKey, data.apiSecret];
+  } catch (error) {
+    console.error("Error con JSON");
+    return [];
+  }
+};
+
+const main = async () => {
+  // Set the request parameters
+  const [apiKey, apiSecret] = await getKeys();
+  //console.log(apiKey, apiSecret);
+  const data = new URLSearchParams();
+  data.append("grant_type", "client_credentials");
+  data.append("client_id", apiKey);
+  data.append("client_secret", apiSecret);
+};
 
 //async function to get the access_token
 async function getToken() {
- const response = await fetch(url, {
+  const response = await fetch(url, {
     method: "POST",
     body: data,
     headers: {
@@ -27,7 +39,7 @@ async function getToken() {
 }
 
 //async function to make the call to the API
-export async function info () {
+async function info() {
   const token = await getToken();
   const response = await fetch(
     "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2023-06-17&adults=1&nonStop=false&max=250",
@@ -40,11 +52,13 @@ export async function info () {
   );
 
   const data2 = await response.json();
-  return data2.data
-};
+  console.log(data2.data);
+}
+
+main();
 
 //* the code below doesnt works because its not async code
-const infoVuelo = info();
-let price = infoVuelo.price.total 
-console.log(infoVuelo);
+// const infoVuelo = info();
+// let price = infoVuelo.price.total;
+// console.log(infoVuelo);
 // we have to create an object (promise) line 26
